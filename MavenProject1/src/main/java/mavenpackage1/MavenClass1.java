69d6cec3537daaed3be4b19ac1a2bd59e5023bc4
ASUS <ASUS@LAPTOP-41UOQFV0>
2025-02-18 21:06:17 +0530

    Initial commit

---
+++ MavenProject1/src/main/java/mavenpackage1/MavenClass1.java
@@ -0,0 +1,33 @@
+package mavenpackage1;
+
+public class MavenClass1 {
+int a;
+	public static void main(String[] args) {
+		System.out.println(MavenClass1.param1(2,3));
+		MavenClass1 mc=new MavenClass1("Hello");
+		MavenClass1 mc1=new MavenClass1(5);
+	mc1.instancemethod(3);
+		
+	}
+	public static int param1(int a , int b)
+	{
+		int c=a+b;
+		return c;
+	}
+ public MavenClass1(String s)
+ {
+	
+	System.out.println(s);
+ }
+ public MavenClass1(int a)
+ {
+	this.a=a;
+	System.out.println(a);
+ }
+ public void instancemethod( int b)
+ {
+	int m=a*b;
+	System.out.println(m);
+ }
+ 
+}
